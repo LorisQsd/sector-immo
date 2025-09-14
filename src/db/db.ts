78b92ef0eab157d2ac564614375ead574cdf73cc
Loris QUESADO <loris.quesado@hotmail.fr>
2025-09-14 17:24:30 +0200

    Replace env name

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,9 +2,9 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema/auth.schema";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
+if (!process.env.POSTGRES_URL) {
+  throw new Error("POSTGRES_URL is not set");
 }
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.POSTGRES_URL);
 export const db = drizzle({ client: sql, schema });
