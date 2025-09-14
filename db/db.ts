5decb32707675107336a7317dac98753f51041f5
Loris QUESADO <loris.quesado@hotmail.fr>
2025-09-14 12:42:22 +0200

    init drizzle schemas

---
+++ db/db.ts
@@ -0,0 +1,12 @@
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/neon-http";
+
+config({ path: ".env.development.local" }); // or .env.local
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const sql = neon(process.env.DATABASE_URL);
+export const db = drizzle({ client: sql });
